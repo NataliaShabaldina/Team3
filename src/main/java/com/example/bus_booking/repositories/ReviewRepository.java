479ffb0675f0857d7d27eff4e439577b90f11238
Vladimir <vladimir118311@gmail.com>
2024-12-07 17:07:52 +0300

    Репозиторий отзывов

--- src/main/java/com/example/bus_booking/repositories/ReviewRepository.java
+++ src/main/java/com/example/bus_booking/repositories/ReviewRepository.java
@@ -1,5 +1,16 @@
 package com.example.bus_booking.repositories;
 
-public interface ReviewRepository {
+import com.example.bus_booking.entities.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("SELECT n FROM Review n WHERE n.client.id = :clientId AND n.review = :review")
+    List<Review> findByClientIdAndReview(@Param("clientId") Long clientId, @Param("review") Review review);
+
+    @Query("SELECT n FROM Review n WHERE n.bus.id = :busId AND n.review = :review")
+    List<Review> findByBusIdAndReview(@Param("busId") Long busId, @Param("review") Review review);
 }
