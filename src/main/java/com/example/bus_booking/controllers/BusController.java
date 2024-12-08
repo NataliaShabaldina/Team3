a7741ed7b2704fd0beceda9a9b53e0378f44cbb3
Vladimir <rainofcastomer@gmail.com>
2024-12-08 18:07:03 +0300

    Контроллеры client и bus

--- src/main/java/com/example/bus_booking/controllers/BusController.java
+++ src/main/java/com/example/bus_booking/controllers/BusController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ClientController {
-    @GetMapping("/client")
-    public String client() {
-        return "entities/Client";
+public class BusController {
+    @GetMapping("/bus")
+    public String bus() {
+        return "entities/bus";
     }
 }
--- src/main/java/com/example/bus_booking/repositories/ClientRepository.java
+++ src/main/java/com/example/bus_booking/repositories/ClientRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    //поиск клиента по еmail
+    //поиск клиента по email
     Client findByEmail(String email);
 
     //поиск клиента по номеру телефона
