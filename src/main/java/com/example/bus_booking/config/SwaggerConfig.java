604dce1d6ee740213c07e7ce96d7f9a41622e540
Linaz <zellik122711@mail.ru>
2024-12-17 16:38:40 +0300

    swaggerConfig

--- src/main/java/com/example/bus_booking/config/SwaggerConfig.java
+++ src/main/java/com/example/bus_booking/config/SwaggerConfig.java
@@ -1,16 +1,18 @@
 package com.example.bus_booking.config;
 
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public GroupedOpenApi customOpenAPI() {
-        return GroupedOpenApi.builder()
-                .group("public")
-                .pathsToMatch("/api/**")
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bus Booking API")
+                        .version("1.0")
+                        .description("API для системы бронирования автобусов"));
     }
 }
